3df3e827be26ebe117132852eec7f954059253e2
HAOTIAN ZHAN <hz2851@nyu.edu>
2024-06-08 20:24:55 -0400

    Leaphand

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='open-teach',
-    version='1.0.0',
-    packages=find_packages(),
-    description='Open-Teach:VR Teleoperation for Robotic Manipulation',
-   
-)
